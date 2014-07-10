5645c735670d72221121f6ada23ce0c1ee4b3999
the greatest <pherbercerhern@gmail.com>
2014-07-10 10:18:51 -1000

    index page

---
+++ default.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+<?php
+include('header.php');
+?>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php 
+echo $obj->title;
+?>/index</title>
+
+    <!-- Bootstrap -->
+    <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+
+
+    <!-- SKIN -->
+   <link href="skins/takebackthecity/default.css" rel="stylesheet">
+    <![endif]-->
+  </head>
+  <body>
+
+<center><div class="bannanar">SOME KIND OF IMAGE/BANNER/SOMETHING HERE?</div></center><br><br>
+
+
+<div class="row">
+  <div class="col-xs-6"><div class="meerkat"><ul class="nav nav-pills nav-stacked">
+  <li class="active"><a href="/default.php">Portal</a></li>
+  <li><a href="/news/overview.php">News</a></li>
+  <li><a href="/gallery/overview.php">Gallery</a></li>
+  <li><a href="/admin/login.php">Login</a></li>
+  <li><a href="/portal/documentation.php">Documentation</a></li>
+  <li><a href="/portal/credits.php">Credits</a></li>
+</ul></div></div>
+  <div class="col-xs-6"><div class="tigerlion"><h1>Profile</h1>
+This is a profile, there are things here. Bippity boppity I like chocolatey <em>*leans away from the mic to breathe*</em>. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever.<br><br> Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Lorem ipsum whatever. Hello. <br><br><br></div></div>
+</div>
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
